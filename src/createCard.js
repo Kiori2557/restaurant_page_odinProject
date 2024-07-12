3dd5b2809c254357e01d1335106feb8bea9c13c1
Aung Khant Paing <akpaing99@gmail.com>
2024-07-12 14:31:10 +0630

    menu card styling  -display dish label on each menu card  -display credit for each img in menu card

--- src/createCard.js
+++ src/createCard.js
@@ -2,6 +2,8 @@ export function createCard(img) {
   img.forEach((item) => {
     let parent;
     let itemDiv = document.createElement("div");
+    let dishLabel = document.createElement("div");
+    let credit = document.createElement("div");
     if (item.category === "breakfast") {
       parent = document.querySelector(".breakfast");
     } else if (item.category === "lunch") {
@@ -11,6 +13,12 @@ export function createCard(img) {
     } else if (item.category === "juice") {
       parent = document.querySelector(".juice");
     }
+    credit.innerHTML = item.credit;
+    credit.classList.add("credit");
+    dishLabel.textContent = item.name;
+    dishLabel.classList.add("dishLabel");
+    itemDiv.appendChild(dishLabel);
+    itemDiv.appendChild(credit);
     itemDiv.style.background = `center/ cover no-repeat url("${item.path}")`;
     itemDiv.classList.add("menuCard");
     parent.appendChild(itemDiv);
